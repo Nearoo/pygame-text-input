03084b0d5a2e34a01c63b00f6f8da4fa57fd3cd7
Silas Gyger <sgyger@student.ethz.ch>
2019-10-13 12:04:24 +0200

    added example for faster testing

--- pygame_textinput.py
+++ pygame_textinput.py
@@ -177,3 +177,30 @@ class TextInput:
     def clear_text(self):
         self.input_string = ""
         self.cursor_position = 0
+
+
+
+if __name__ == "__main__":
+    pygame.init()
+
+    # Create TextInput-object
+    textinput = TextInput()
+
+    screen = pygame.display.set_mode((1000, 200))
+    clock = pygame.time.Clock()
+
+    while True:
+        screen.fill((225, 225, 225))
+
+        events = pygame.event.get()
+        for event in events:
+            if event.type == pygame.QUIT:
+                exit()
+
+        # Feed it with events every frame
+        textinput.update(events)
+        # Blit its surface onto the screen
+        screen.blit(textinput.get_surface(), (10, 10))
+
+        pygame.display.update()
+        clock.tick(30)
